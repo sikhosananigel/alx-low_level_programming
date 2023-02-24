c86825a68f33217688fa4d200b3aa2c1ceb2d357
sikhosananigel <sikhosananigel@gmail.com>
2023-02-24 14:47:35 -0800

    x14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,14 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 14; y++)
-		{
-			if (y > 9)
-				_putchar((y / 10) + '0');
-
-		}
-				_putchar((y % 10) + '0');
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
+	}
 }
